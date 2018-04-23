fc487d1f744c3a84713f5eff3a9e72b805430e1e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/DXPivotGrid_SelectingCellTemplate/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Markup;
+using DevExpress.Xpf.PivotGrid.Internal;
+
+namespace DXPivotGrid_SelectingCellTemplate {
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+            picotGridControl1.DataSource =
+                new nwindDataSetTableAdapters.SalesPersonTableAdapter().GetData();
+        }
+    }
+    public class CellTemplateSelector : DataTemplateSelector {
+        public override DataTemplate SelectTemplate(object item, DependencyObject container) {
+            Window mainWindow = Application.Current.MainWindow;
+            CellsAreaItem cell = (CellsAreaItem)item;
+
+            // Calculates the share of a cell value in the Row Grand Total value.
+            double share = Convert.ToDouble(cell.Value) / Convert.ToDouble(cell.RowTotalValue);
+
+            // Applies the Default template to the Row Grand Total cells.
+            if (cell.RowValue == null)
+                return mainWindow.FindResource("DefaultCellTemplate") as DataTemplate;
+
+            // If the share is too far from 50%, the Highlighted template is selected.
+            // Otherwise, the Normal template is applied to the cell.
+            if (share > 0.7 || share < 0.3)
+                return mainWindow.FindResource("HighlightedCellTemplate") as DataTemplate;
+            else
+                return mainWindow.FindResource("NormalCellTemplate") as DataTemplate;
+        }
+    }
+    public class RoundConverter : MarkupExtension, IValueConverter {
+        #region IValueConverter Members
+        public object Convert(object value, Type targetType,
+                              object parameter, CultureInfo culture) {
+            return System.Convert.ToInt32(value);
+        }
+        public object ConvertBack(object value, Type targetType,
+                                  object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+        #endregion
+        public override object ProvideValue(IServiceProvider serviceProvider) {
+            return this;
+        }
+    }
+}
---
+++ VB/DXPivotGrid_SelectingCellTemplate/MainWindow.xaml.vb
@@ -0,0 +1,62 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Globalization
+Imports System.Windows
+Imports System.Windows.Controls
+Imports System.Windows.Data
+Imports System.Windows.Markup
+Imports DevExpress.Xpf.PivotGrid.Internal
+
+Namespace DXPivotGrid_SelectingCellTemplate
+	Partial Public Class MainWindow
+		Inherits Window
+		Public Sub New()
+			InitializeComponent()
+			picotGridControl1.DataSource = _
+				New nwindDataSetTableAdapters.SalesPersonTableAdapter().GetData()
+		End Sub
+	End Class
+	Public Class CellTemplateSelector
+		Inherits DataTemplateSelector
+		Public Overrides Function SelectTemplate(ByVal item As Object, _
+				ByVal container As DependencyObject) As DataTemplate
+			Dim mainWindow As Window = Application.Current.MainWindow
+			Dim cell As CellsAreaItem = CType(item, CellsAreaItem)
+
+			' Calculates the share of a cell value in the Row Grand Total value.
+			Dim share As Double = Convert.ToDouble(cell.Value) / Convert.ToDouble(cell.RowTotalValue)
+
+			' Applies the Default template to the Row Grand Total cells.
+			If cell.RowValue Is Nothing Then
+				Return TryCast(mainWindow.FindResource("DefaultCellTemplate"), DataTemplate)
+			End If
+
+			' If the share is too far from 50%, the Highlighted template is selected.
+			' Otherwise, the Normal template is applied to the cell.
+			If share > 0.7 OrElse share < 0.3 Then
+				Return TryCast(mainWindow.FindResource("HighlightedCellTemplate"), DataTemplate)
+			Else
+				Return TryCast(mainWindow.FindResource("NormalCellTemplate"), DataTemplate)
+			End If
+		End Function
+	End Class
+	Public Class RoundConverter
+		Inherits MarkupExtension
+		Implements IValueConverter
+		#Region "IValueConverter Members"
+		Public Function Convert(ByVal value As Object, ByVal targetType As Type, _
+				ByVal parameter As Object, ByVal culture As CultureInfo) As Object _
+				Implements IValueConverter.Convert
+			Return System.Convert.ToInt32(value)
+		End Function
+		Public Function ConvertBack(ByVal value As Object, ByVal targetType As Type, _
+				ByVal parameter As Object, ByVal culture As CultureInfo) As Object _
+				Implements IValueConverter.ConvertBack
+			Throw New NotImplementedException()
+		End Function
+		#End Region
+		Public Overrides Function ProvideValue(ByVal serviceProvider As IServiceProvider) As Object
+			Return Me
+		End Function
+	End Class
+End Namespace
